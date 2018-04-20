235ccf0a197c81fd982264d9dd956fbaadb91ffe
nuchnat khammona <nuchnat0221@gmail.com>
2018-04-20 13:10:19 +0700

    add oil_price

---
+++ oil_price.py
@@ -0,0 +1,18 @@
+from zeep import Client
+from lxml import etree
+
+def get_prices():
+    client = Client('http://www.pttplc.com/webservice/pttinfo.asmx?WSDL')
+    result = client.service.CurrentOilPrice("en")
+    root = etree.fromstring(result)
+    prices = []
+    for r in root.xpath('DataAccess'):
+        product = r.xpath('PRODUCT/text()')[0]
+        price = r.xpath('PRICE/text()') or [0]
+        prices.append( [product, float(price[0])] )
+        # print(product,float(price[0]),' BAHT')
+    return prices
+
+if __name__ == '__main__':
+    l = get_prices()
+    print(l)
