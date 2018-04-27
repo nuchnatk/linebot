1cca7860a6aef4bc145d5b6d4da21416bc1e0936
nuchnat khammona <nuchnat0221@gmail.com>
2018-04-27 15:05:33 +0700

    Add car analytics feature

--- app.py
+++ app.py
@@ -1,3 +1,9 @@
+import os
+import sys
+from features.CarAnalytics import LicencePlate
+import tempfile
+
+
 from flask import Flask, request, abort
 
 from linebot import (
@@ -7,11 +13,15 @@ from linebot.exceptions import (
     InvalidSignatureError
 )
 from linebot.models import (
-    MessageEvent, TextMessage, TextSendMessage,
+    MessageEvent, TextMessage, TextSendMessage, ImageMessage, VideoMessage, AudioMessage
 )
 
 import oil_price
 app = Flask(__name__)
+
+latest_image_path = ""
+
+static_tmp_path = os.path.join(os.path.dirname(__file__), 'static', 'tmp')
 
 
 line_bot_api = LineBotApi('C6yfsXHC7V9w4hirs8TLebWIBFVukTMf8akta0E1y86Js6PXYJ5OcIAQIfRF7K+/ANoCEWyiq8AflFKh12A6qNkrKfVrZjSGAIbCszIWX+Mj9BX1jBqF2kENu5y0H96zQckb79g0FEzSllODk/iacgdB04t89/1O/w1cDnyilFU=')
@@ -40,6 +50,8 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
+    global latest_image_path
+
     if event.message.text == 'ราคาน้ำมัน' :
         l = oil_price.get_prices()
         s = ""
@@ -48,11 +60,71 @@ def handle_message(event):
         line_bot_api.reply_message(
             event.reply_token,
             TextSendMessage(text=s))
+    elif event.message.text == 'วิเคราะห์รูป':
+        line_bot_api.reply_message(
+            event.reply_token, [
+                TextSendMessage(text='สักครู่ครับ')
+            ])
+
+        try:
+            lp = LicencePlate()
+            result = lp.process(latest_image_path)
+            s= lp.translate(result)
+
+            line_bot_api.push_message(
+                event.source.user_id, [
+                TextSendMessage(text=s)
+            ])
+        except Exception as e:
+            print('Exception:',type(e),e)
+            line_bot_api.push_message(
+                event.source.user_id, [
+                    TextSendMessage(text='ไม่สามารถวิเคราะห์รูปได้')
+                ])
     else:
         line_bot_api.reply_message(
             event.reply_token,
             TextSendMessage(text=event.message.text+'ขอรับ'))
         
+
+
+
+
+# Other Message Type
+@handler.add(MessageEvent, message=(ImageMessage, VideoMessage, AudioMessage))
+def handle_content_message(event):
+    global latest_image_path
+
+    if isinstance(event.message, ImageMessage):
+        ext = 'jpg'
+    elif isinstance(event.message, VideoMessage):
+        ext = 'mp4'
+    elif isinstance(event.message, AudioMessage):
+        ext = 'm4a'
+    else:
+        return
+
+    message_content = line_bot_api.get_message_content(event.message.id)
+    with tempfile.NamedTemporaryFile(dir=static_tmp_path, prefix=ext + '-', delete=False) as tf:
+        for chunk in message_content.iter_content():
+            tf.write(chunk)
+        tempfile_path = tf.name
+
+    dist_path = tempfile_path + '.' + ext
+    dist_name = os.path.basename(dist_path)
+    os.rename(tempfile_path, dist_path)
+
+    latest_image_path = dist_path
+    line_bot_api.reply_message(
+        event.reply_token, [
+            TextSendMessage(text='เก็บรูปให้แล้วค่ะ')
+        ])
+
+    # line_bot_api.reply_message(
+    #     event.reply_token, [
+    #         TextSendMessage(text='Save content.'),
+    #         TextSendMessage(text=request.host_url + os.path.join('static', 'tmp', dist_name))
+    #     ])
 
 
 if __name__ == "__main__":
